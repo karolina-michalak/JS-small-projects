8d924aca6ff64bf5b42ba4fa892ed5c65ab6ae93
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-29 18:08:03 +0100

    adding items function improved

--- task-list/app.js
+++ task-list/app.js
@@ -4,24 +4,28 @@ const clearBtn = document.querySelector('.clear-tasks')
 const filter = document.querySelector('#filter')
 const taskInput = document.querySelector('#task')
 
-loadEventListeners()
-
 function loadEventListeners(){
     form.addEventListener('submit', addTask)
 }
 
 function addTask(e){
     if(taskInput.value === ''){
-        alert('Add task')
+        alert('type task')
     }
-    const li = document.createElement('li')
-    li.className = 'collection-item'
-    li.appendChild(document.createTextNode(taskInput.value))
-    const link = document.createElement('a')
-    link.className = 'delete-item secondary-content'
-    link.innerHTML = '<i class="fa fa-remove"></i>'
-    li.appendChild(link)
-    taskList.appendChild(li)
-    taskInput.value = ''
-    e.preventDefault()
+    else{
+        const li = document.createElement('li')
+        li.className = 'collection-item'
+        li.appendChild(document.createTextNode(taskInput.value))
+        const link = document.createElement('a')
+        link.className = 'delete-item secondary-content'
+        link.innerHTML = '<i class="fa fa-remove"></i>'
+        li.appendChild(link)
+        taskList.appendChild(li)
+        taskInput.value = ''
+        e.preventDefault()
+    }
 }
+
+loadEventListeners()
+
+
