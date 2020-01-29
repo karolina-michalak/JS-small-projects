56a3505d7fd2e9c6861a384d59ff92f665f49801
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-29 22:03:31 +0100

    assigned variables

---
+++ number-guesser/index.js
@@ -0,0 +1,12 @@
+let min = 1;
+let max = 10;
+let winningNum = 2;
+let guessesLeft = 3;
+
+const game = document.getElementById('game');
+const minNum = document.querySelector('.min-num');
+const maxNum = document.querySelector('.max-num');
+const btn = document.querySelector('#guess-btn');
+const input = document.querySelector('#guess-input');
+
+
