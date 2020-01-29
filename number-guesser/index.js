ca00c1ff0f3db5322bfc03c053282a0955a3c4f8
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-29 22:18:18 +0100

    validation and winning case

--- number-guesser/index.js
+++ number-guesser/index.js
@@ -8,5 +8,26 @@ const minNum = document.querySelector('.min-num');
 const maxNum = document.querySelector('.max-num');
 const btn = document.querySelector('#guess-btn');
 const input = document.querySelector('#guess-input');
+const message = document.querySelector('.message')
 
+minNum.textContent = min;
+maxNum.textContent = max;
 
+btn.addEventListener('click', function(){
+    let guess = parseInt(input.value)
+    if(isNaN(guess) || guess < min || guess > max){
+        setMessage(`Please enter a number between ${min} and ${max}`, `red`)
+    }
+    if(guess === winningNum){
+        input.disabled = true
+        input.style.borderColor = 'green'
+        setMessage(`${winningNum} is correct`, `green`)
+    } else {
+
+    }
+})
+
+setMessage = (msg, color) => {
+    message.style.color = color
+    message.textContent = msg
+}
