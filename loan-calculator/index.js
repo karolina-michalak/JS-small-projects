d903711318760ec6a60e6aefb05b135c099608ac
Karolina Michalak <karolina0michalak@gmail.com>
2020-01-29 21:31:05 +0100

    added loader

--- loan-calculator/index.js
+++ loan-calculator/index.js
@@ -1,7 +1,12 @@
-document.getElementById('loan-form').addEventListener('submit', calculateResutls)
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    document.getElementById('results').style.display = 'none'
+    document.getElementById('loading').style.display = 'block'
+    setTimeout(calculateResutls, 2000)
 
-function calculateResutls(e) {
     e.preventDefault()
+})
+
+function calculateResutls() {
     const amount = document.getElementById('amount')
     const interest = document.getElementById('interest')
     const years = document.getElementById('years')
@@ -20,12 +25,17 @@ function calculateResutls(e) {
         monthlyPayment.value = monthly.toFixed(2)
         totalPayment.value = (monthly * calculatedPayments).toFixed(2)
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2)
+        document.getElementById('results').style.display = 'block'
+        document.getElementById('loading').style.display = 'none'
+
     } else {
         showError('Please fill in all the fields')
     }
 }
 
 showError = error => {
+    document.getElementById('results').style.display = 'none'
+    document.getElementById('loading').style.display = 'none'
     blockButton()
     const div = document.createElement('div')
     const card = document.querySelector('.card')
