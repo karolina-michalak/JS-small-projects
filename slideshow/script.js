08f77ab85dcd0d0741034031128ead98b2e5b6ee
Karolina Michalak <karolina0michalak@gmail.com>
2020-02-05 19:35:04 +0100

    changing image with arrows

--- slideshow/script.js
+++ slideshow/script.js
@@ -1,4 +1,31 @@
-const container = document.querySelector('.slider-container');
-const slides = document.querySelector('.slides');
+const img = document.querySelectorAll('img');
+const container = document.querySelector('.container');
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
+let index = 0;
+
+
+changeImage = () => {
+    if (index > img.length - 1) {
+        index = 0;
+    }
+    if (index < 0) {
+        index = img.length - 1;
+    }
+    img.forEach((el) => {
+        el.style.display = 'none';
+    });
+    img[index].style.display = 'block';
+}
+
+next.addEventListener('click', () => {
+    index += 1;
+    changeImage();
+});
+
+prev.addEventListener('click', () => {
+    index -= 1;
+    changeImage();
+});
+
+changeImage()
