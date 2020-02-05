27c44df6f8246075c0998536a1fa89797246851e
Karolina Michalak <karolina0michalak@gmail.com>
2020-02-05 18:45:30 +0100

    added diffrent styling

--- slideshow/script.js
+++ slideshow/script.js
@@ -1,28 +1,4 @@
-let slides = document.querySelectorAll('.slide');
+const container = document.querySelector('.slider-container');
+const slides = document.querySelector('.slides');
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
-let slideIndex = 0;
-
-changePhoto = () => {
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1
-    }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(changePhoto, 10000);
-}
-
-prev.addEventListener('click', () => {
-    slideIndex -= 1;
-    changePhoto()
-})
-
-next.addEventListener('click', () => {
-    slideIndex += 1;
-    changePhoto()
-})
-
-window.onload = changePhoto
